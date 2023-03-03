e92c40602dfb04557595876e848ac2e6804050d4
ErickDC <erickdiazc717@gmail.com>
2023-03-03 13:39:14 -0500

    functions added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import charmander from '../images/charmander.png';
 
-const Card = () => {
+const Card = ({ pokemon, loading, infoPokemon }) => {
     return (
         <>
-            <div className="card">
-                <h2>1</h2>
-                <img src={charmander} alt="charmander" />
-                <h2>Charmander</h2>
-            </div>
+            {
+                loading
+                    ? <h1> Loading...</h1>
+                    : pokemon.map((item) => {
+                        return (
+                            <>
+                                <div className="card" key={item.id} onClick={() => infoPokemon(item)}>
+                                    <h2>{item.id}</h2>
+                                    <img src={item.sprites.front_default} alt={item.name} />
+                                    <h2>{item.name}</h2>
+                                </div>
+                            </>
+                        )
+                    })
+            }
         </>
     )
 }
